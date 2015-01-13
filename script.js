e42164eb5901b29ab36f9368224228c28ca7a142
Doug Sebastian <dasebast@gmail.com>
2015-01-12 17:11:27 -0700

    push war

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ $(document).ready(function() {
 	var convert_value_to_string = function(value) {
 		if (value > 10) {
 			switch (value) {
-				case 11:
+			case 11:
 				return 'Jack';
 				break;
-				case 12:
+			case 12:
 				return 'Queen';
 				break;
-				case 13:
+			case 13:
 				return 'King';
 				break;
 			}
 		}
 		return value.toString();
-	}
+	};
 
 	//what does this do?
 	var deck = [];
@@ -26,7 +26,22 @@ $(document).ready(function() {
 		for (var j = 0; j<13; j++) {
 			deck.push({number: j+1, suit: suit});
 		}
-	}
+	};
+
+	// deck = [
+	// 	{
+	// 		suit: "hearts".
+	// 		number: 1
+
+	// 	}
+	// 	{
+	// 		suit: "hearts",
+	// 		number: 2
+	// 	}
+
+
+
+	// ]
 	
 	//what does this do?
 	var shuffle = function(array) { 
@@ -44,17 +59,41 @@ $(document).ready(function() {
 	}
 	
 	//Now call the shuffle function and save the result of what shuffle returns into your deck variable
-	
+	deck = shuffle(deck);
+
+
 	var cards_player_1 = [];
 	var cards_player_2 = [];
 	// write a function called deal that will evently divide the deck up between the two players
 	
+	var deal = function(deckName){
+		for (var i = 0; i < deckName.length; i++) { 
+			if (i % 2 === 0) {
+				cards_player_1.push(deckName[i]);
+			} else {
+				cards_player_2.push(deckName[i]);
+			}
+		}
+	};
 	
+	deal(deck);
+
 	//create a function (algorithm) called "war" that takes two cards as parameters, compares them and returns a winner. A tie should return false.
-	var war = function(){
-	
+	var war = function(card1, card2){
 		
-	}
+		if (card1.number > card2.number) {
+			return "Player 1 wins!";
+		} else if (card2.number > card1.number) {
+			return "Player 2 wins!";
+		} else if (card1.number === card2.number) {
+			return "Tie!";
+		}
+	
+	};
+
+	
+
+
 	
 	var advance = function(){
 		//take the top two cards and display them
@@ -73,8 +112,38 @@ $(document).ready(function() {
 	//create a play function
 		//compare the cards
 		//give the winner both cards (at end of deck)
+		var checker = 52;
 	var play = function(){
+		if(checker !== cards_player_1.length + cards_player_2.length){
+			alert("stop");
+		}
+		var winner = war(cards_player_1[0], cards_player_2[0]);
+		if (winner === "Player 1 wins!") {
+			console.log("player 1 wins");
+			cards_player_1.push(cards_player_1.shift());
+			cards_player_1.push(cards_player_2.shift());
+		} else if (winner === "Player 2 wins!") {
+			console.log("player 2 wins");
+				cards_player_2.push(cards_player_1.shift());
+				cards_player_2.push(cards_player_2.shift());
+		} else if (winner === "Tie!") {
+			console.log("tie");
+			var winnerTie = war(cards_player_1[3], cards_player_2[3]);
+			if (winnerTie === "Player 1 wins!") {
+				cards_player_1 = cards_player_1.concat(cards_player_1.splice(0, 4));
+				cards_player_1 = cards_player_1.concat(cards_player_2.splice(0, 4));
+			} else if (winnerTie === "Player 2 wins!") {
+				cards_player_2 = cards_player_2.concat(cards_player_1.splice(0, 4));
+				cards_player_2 = cards_player_2.concat(cards_player_2.splice(0, 4));
+			} else {
+				cards_player_2.push(cards_player_2.shift());
+				cards_player_1.push(cards_player_1.shift());
+			}
+		}
 		
+
+
+
 		//this function (defined below) will continue to the next turn
 		advance();
 	}
